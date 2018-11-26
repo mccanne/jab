9aff445ea48f3b48596d542105f41eb157ff1fac
Steven McCanne <mccanne@hatch.local>
2018-11-25 17:17:28 -0800

    bug fixes

--- jab.go
+++ jab.go
@@ -2,9 +2,11 @@ package jab
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"reflect"
+	"strings"
+
+	"github.com/kr/pretty"
 )
 
 type Matcher interface {
@@ -23,7 +25,7 @@ type Binding struct {
 }
 
 type Template struct {
-	Bindings []Binding
+	Bindings []*Binding
 }
 
 func NewTemplate() *Template {
@@ -45,35 +47,47 @@ func (matcher FieldMatcher) match(node map[string]interface{}) bool {
 
 func (t *Template) MatchKey(key, value string, typ interface{}) *Binding {
 	binding := &Binding{FieldMatcher{key, value}, typ, nil}
-	t.Bindings = append(t.Bindings, *binding)
+	t.Bindings = append(t.Bindings, binding)
 	return binding
 }
 
 func (t *Template) match(node map[string]interface{}) *Binding {
 	for _, binding := range t.Bindings {
 		if binding.match(node) {
-			return &binding
+			return binding
 		}
 	}
 	return nil
 }
 
-func (b *Binding) AddChild() *Template {
-	t := NewTemplate()
-	b.Children = append(b.Children, Child{})
-	return t
+func (b *Binding) ChildObject(key string, t *Template) *Binding {
+	b.Children = append(b.Children, Child{key, t})
+	return b
 }
 
-func parse(node map[string]interface{}, t *Template) (interface{}, error) {
+func old(node map[string]interface{}, t *Template) (interface{}, error) {
+
+	pretty.Println("PARSE")
+	pretty.Println(node)
+	pretty.Println(t)
+
 	match := t.match(node)
+
 	if match == nil {
+		pretty.Println("NO MATCH")
+		pretty.Println(node)
+		pretty.Println(t)
 		return nil, nil
 	}
+	pretty.Println("MATCH & MAKE")
+	pretty.Println(match.Struct)
 	instance := reflect.New(reflect.TypeOf(match.Struct))
+	parent := instance.Elem()
+
 	for _, child := range match.Children {
 		childNode, ok := node[child.Field]
 		if !ok {
-			err := fmt.Errorf("missing child: %s", child.Field)
+			err := fmt.Errorf("missing child %s for struct %T", child.Field, match.Struct)
 			return nil, err
 		}
 		childMap, ok := childNode.(map[string]interface{})
@@ -81,16 +95,101 @@ func parse(node map[string]interface{}, t *Template) (interface{}, error) {
 			err := fmt.Errorf("field %s is not an object", child.Field)
 			return nil, err
 		}
+		pretty.Println("PARSE CHILD")
 		childInstance, err := parse(childMap, child.Template)
 		if err != nil {
 			return nil, err
 		}
-		field := instance.FieldByName(child.Field)
-		field.Set(reflect.ValueOf(childInstance))
+
+		fname := child.Field
+		field := parent.FieldByName(fname)
+		if !field.IsValid() {
+			fname = strings.Title(fname)
+			field = parent.FieldByName(fname)
+		}
+
+		pretty.Println("SETTING")
+		pretty.Println("===")
+		pretty.Println(fname)
+		pretty.Println(match.Struct)
+		pretty.Println(childInstance)
+		pretty.Println(reflect.ValueOf(childInstance).Elem())
+		field.Set(reflect.ValueOf(childInstance).Elem())
+		pretty.Println("===")
 	}
 	return instance.Interface(), nil
 }
 
+var nilValue reflect.Value
+
+func parse(node map[string]interface{}, t *Template) (reflect.Value, error) {
+
+	pretty.Println("PARSE")
+	pretty.Println(node)
+	pretty.Println(t)
+
+	match := t.match(node)
+
+	if match == nil {
+		pretty.Println("NO MATCH")
+		pretty.Println(node)
+		pretty.Println(t)
+		return nilValue, nil
+	}
+	pretty.Println("MATCH & MAKE")
+	pretty.Println(match.Struct)
+	parentPtr := reflect.New(reflect.TypeOf(match.Struct))
+	parent := parentPtr.Elem()
+
+	for _, child := range match.Children {
+		childNode, ok := node[child.Field]
+		if !ok {
+			err := fmt.Errorf("missing child %s for struct %T", child.Field, match.Struct)
+			return nilValue, err
+		}
+		childMap, ok := childNode.(map[string]interface{})
+		if !ok {
+			err := fmt.Errorf("field %s is not an object", child.Field)
+			return nilValue, err
+		}
+		pretty.Println("PARSE CHILD")
+		childVal, err := parse(childMap, child.Template)
+		if err != nil {
+			return nilValue, err
+		}
+		if childVal == nilValue {
+			continue
+		}
+
+		fname := child.Field
+		field := parent.FieldByName(fname)
+		if !field.IsValid() {
+			fname = strings.Title(fname)
+			field = parent.FieldByName(fname)
+		}
+
+		pretty.Println("SETTING")
+		pretty.Println("===")
+		pretty.Println(fname)
+		pretty.Println(childVal.Interface())
+		pretty.Println("PARENT-PRE-SET")
+		pretty.Println(parent.Interface())
+		pretty.Println(field.Type().Kind().String())
+		pretty.Println(childVal.Type().Kind().String())
+		if field.Type().Kind().String() == "interface" {
+			field.Set(childVal.Addr())
+		} else {
+			field.Set(childVal)
+		}
+		pretty.Println("PARENT-POST-SET")
+		pretty.Println(parent.Interface())
+		pretty.Println("===")
+	}
+	//return parentPtr, nil
+	return parent, nil
+}
+
+/*
 func parseArray(a []interface{}, t *Template) ([]interface{}, error) {
 	for k, v := range a {
 		o, ok := v.(map[string]interface{})
@@ -104,6 +203,7 @@ func parseArray(a []interface{}, t *Template) ([]interface{}, error) {
 	}
 	return a, nil
 }
+*/
 
 func Parse(b []byte, t *Template) (interface{}, error) {
 	var root interface{}
@@ -112,25 +212,58 @@ func Parse(b []byte, t *Template) (interface{}, error) {
 		return nil, err
 	}
 	object, ok := root.(map[string]interface{})
-	var out interface{}
+	var out reflect.Value
 	if ok {
 		out, err = parse(object, t)
 		if err != nil {
 			return nil, err
 		}
 	} else {
-		a, ok := root.([]interface{})
-		if !ok {
-			return nil, errors.New("input is either a JSON object or a JSON array of objects")
-		}
-		out, err = parseArray(a, t)
-		if err != nil {
-			return nil, err
-		}
+		/*
+			a, ok := root.([]interface{})
+			if !ok {
+				return nil, errors.New("input is either a JSON object or a JSON array of objects")
+			}
+			out, err = parseArray(a, t)
+			if err != nil {
+				return nil, err
+			}*/
 	}
-	err = json.Unmarshal(b, &out)
+	v := out.Addr().Interface()
+	pretty.Println("FINAL UNMARSHALL")
+	pretty.Println(string(b))
+	pretty.Println(v)
+	err = json.Unmarshal(b, &v)
 	if err != nil {
 		return nil, err
 	}
-	return out, nil
+	pretty.Println(v)
+	return v, nil
+}
+
+func Parsex(b []byte, t *Template) (interface{}, error) {
+	var root interface{}
+	err := json.Unmarshal(b, &root)
+	if err != nil {
+		return nil, err
+	}
+	object, ok := root.(map[string]interface{})
+	var out reflect.Value
+	if ok {
+		out, err = parse(object, t)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		/*
+			a, ok := root.([]interface{})
+			if !ok {
+				return nil, errors.New("input is either a JSON object or a JSON array of objects")
+			}
+			out, err = parseArray(a, t)
+			if err != nil {
+				return nil, err
+			}*/
+	}
+	return out.Interface(), nil
 }
--- jab_test.go
+++ jab_test.go
@@ -1,7 +1,9 @@
 package jab_test
 
 import (
+	"encoding/json"
 	"fmt"
+	"reflect"
 	"testing"
 
 	"github.com/kr/pretty"
@@ -18,46 +20,15 @@ type Bar struct {
 	Value string
 }
 
-//type Node interface{}
-
-//type Leaf struct {
-//	Value
-//}
-
-//func genSimple() *jab.Template {
-//	template := jab.NewTemplate()
-//	foo := template.Match("foo", Foo{})
-//	bar := template.Match("bar", Bar{})
-//	baz := template.Match("baz", Baz{})
-//	baz.AddChild("A").Match("foo", foo)
-//	baz.AddChild("B").Match("bar", bar)
-//	return template
-
-func test1() {
-	s := `{
-		"Op": "foo",
-		"value": 12
-	}`
-	template := jab.NewTemplate()
-	template.MatchKey("Op", "foo", Foo{})
-	test(s, template)
+type Baz struct {
+	Op string
+	A  Foo
+	B  Bar
 }
 
-func test2() {
-	s := `[
-			{
-				"Op": "foo",
-				"value": 12
-			},
-		{
-			"Op": "bar",
-			"value": "hello"
-		}
-
-	]`
-	template := jab.NewTemplate()
-	template.MatchKey("Op", "foo", Foo{})
-	test(s, template)
+type Bug struct {
+	Op string
+	A  Foo
 }
 
 func test(s string, template *jab.Template) {
@@ -69,9 +40,104 @@ func test(s string, template *jab.Template) {
 	}
 }
 
+func test0() {
+	//	var x interface{} = Bug{}
+	x := Bar{}
+	t := reflect.TypeOf(x)
+	pretty.Println(t)
+	instance := reflect.New(t).Elem()
+	pretty.Println(instance.Kind().String())
+	pretty.Println(instance.FieldByName("A"))
+
+	s3 := `{
+		"op": "bug",
+		"A": {
+			"op": "foo",
+			"value": 13
+		}
+	}`
+
+	template := jab.NewTemplate()
+	template.MatchKey("op", "foo", Foo{})
+	bug := template.MatchKey("op", "bug", Bug{})
+	bug.ChildObject("A", template)
+	test(s3, template)
+}
+
+func test1() {
+	s1 := `{
+		"op": "foo",
+		"value": 12
+	}`
+	s2 := `{
+		"op": "bar",
+		"value": "hello"
+	}`
+	s3 := `{
+		"op": "baz",
+		"A": {
+			"op": "foo",
+			"value": 13
+		},
+		"B": {
+			"op": "bar",
+			"value": "there"
+		}
+	}`
+	/*
+		s4 := `{
+			"op": "baz",
+			"A": {
+				"op": "foo",
+				"value": 13
+			},
+			"B": {
+				"op": "baz",
+				"A": {
+					"op": "foo",
+					"value": 13
+				},
+				"B": {
+					"op": "baz",
+					"value": "oops"
+				}
+			}
+		}`
+	*/
+	template := jab.NewTemplate()
+	template.MatchKey("op", "foo", Foo{})
+	template.MatchKey("op", "bar", Bar{})
+	baz := template.MatchKey("op", "baz", Baz{})
+	baz.ChildObject("A", template)
+	baz.ChildObject("B", template)
+	test(s1, template)
+	test(s2, template)
+	test(s3, template)
+	//(s4, template)
+}
+
+type Node interface {
+	nodeType()
+}
+
+type InternalNode struct {
+	Type  string `json:"type"`
+	Left  Node   `json:"left"`
+	Right Node   `json:"right"`
+}
+
+func (p *InternalNode) nodeType() {}
+func (p *LeafNode) nodeType()     {}
+
+type LeafNode struct {
+	Type  string `json:"type"`
+	Value int    `json:"value"`
+}
+
 const tree = `{
-	"type": "interal",
+	"type": "internal",
 	"left": {
+		"type": "internal",
 		"left": {
 			"type": "leaf",
 			"value": 1
@@ -87,15 +153,75 @@ const tree = `{
 				"value": 5
 			}
 		}
-
 	},
 	"right": {
-		"type": "leaf"
+		"type": "leaf",
 		"value": 7
 	}
 }`
 
+const bug2 = `{
+	"type": "internal",
+	"left": {
+		"type": "leaf",
+		"value": 5
+	},
+	"right": {
+		"type": "leaf",
+		"value": 7
+	}
+}`
+
+func test3() {
+	node := &InternalNode{
+		Type:  "",
+		Left:  &LeafNode{},
+		Right: &LeafNode{}}
+	pretty.Println(node)
+	var v interface{} = node
+	err := json.Unmarshal([]byte(bug2), &v)
+	if err != nil {
+		pretty.Println(err.Error())
+	}
+	pretty.Println(v)
+}
+
+func test2() {
+	template := jab.NewTemplate()
+	template.MatchKey("type", "leaf", LeafNode{})
+	internal := template.MatchKey("type", "internal", InternalNode{})
+	internal.ChildObject("left", template)
+	internal.ChildObject("right", template)
+	//test(tree, template)
+	test(tree, template)
+}
+
+func test4() {
+	template := jab.NewTemplate()
+	template.MatchKey("type", "leaf", LeafNode{})
+	internal := template.MatchKey("type", "internal", InternalNode{})
+	internal.ChildObject("left", template)
+	internal.ChildObject("right", template)
+
+	out, err := jab.Parsex([]byte(bug2), template)
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		pretty.Println(out)
+	}
+	pretty.Println("FINAL UNMARSHALL")
+	pretty.Println(out)
+	err = json.Unmarshal([]byte(bug2), &out)
+	if err != nil {
+		pretty.Println(err.Error())
+	}
+	pretty.Println(out)
+}
+
 func Test_Jab(t *testing.T) {
-	test1()
+	//test0()
+	//test1()
 	test2()
+	//test3()
+	//test4()
 }
